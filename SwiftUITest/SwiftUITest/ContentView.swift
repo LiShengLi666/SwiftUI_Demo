6b5e4e44ae46fa2ca4dc0ebea518a59417ffc465
13554647573@163.com <xili159753>
2020-09-17 21:56:15 +0800

    初始化设置

---
+++ SwiftUITest/SwiftUITest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITest
+//
+//  Created by 李   胜 on 2020/9/17.
+//  Copyright © 2020 Wuhan Xueyetong Big Data  Institute Co. Ltd. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
